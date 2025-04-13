8e5a4770bbe219fb8ee572daa676902a161b903d
Pedro Ramos Paraiso <03ramos011@gmail.com>
2025-04-13 17:38:58 -0300

    mensagem

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,12 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        [
-          'build',
-          'chore',
-          'ci',
-          'docs',
-          'feat',
-          'fix',
-          'perf',
-          'refactor',
-          'revert',
-          'style',
-          'test'
-        ]
-      ],
-      'subject-case': [2, 'always', 'sentence-case'],
-      'header-max-length': [2, 'always', 72],
-      'body-leading-blank': [2, 'always'],
-      'footer-leading-blank': [1, 'always']
-    }
-  };
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', 'sentence-case'],
+    'header-max-length': [2, 'always', 72],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+    ]
+  }
+};
