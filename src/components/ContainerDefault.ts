0d9f2f615295eacf1d1068ea7b90409c81063cec
Pedro Ramos Paraiso <03ramos011@gmail.com>
2025-04-13 15:54:41 -0300

    style(containerdefault): adionado props para o containerDefault
    BREAKING CHANGE: y

--- src/components/ContainerDefault.ts
+++ src/components/ContainerDefault.ts
@@ -1,10 +1,30 @@
 import styled from "styled-components/native";
 
-export const Containerdefault = styled.View`
+interface ContainerdefaultProps {
+  alignItems?: "flex-start" | "center" | "flex-end" | "stretch" | "baseline";
+  justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around" | "space-evenly";
+  flexDirection?: "row" | "column" | "row-reverse" | "column-reverse";
+  flexWrap?: "nowrap" | "wrap" | "wrap-reverse";
+  flex?: number;
+  width?: number | string;
+  height?: number | string;
+  padding?: number;
+  paddingHorizontal?: number;
+  paddingVertical?: number;
+  margin?: number;
+  marginHorizontal?: number;
+  marginVertical?: number;
+  backgroundColor?: string;
+  borderRadius?: number;
+  borderWidth?: number;
+  borderColor?: string;
+}
+
+export const Containerdefault = styled.View<ContainerdefaultProps>`
   background-color: ${({ theme }) => theme.colors.background};
   color: #ffffff;
   padding: 20px;
   height: 100%;
-  align-items: center;
-  justify-content: center;
+  ${({ alignItems }) => alignItems && `align-items: ${alignItems};`}
+  ${({ justifyContent }) => justifyContent && `justify-content: ${justifyContent};`}
 `;
